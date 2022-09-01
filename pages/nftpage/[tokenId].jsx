25e009d3ef556d8119c1daffb8e55067d7a77528
saggemode <saggemode@gmail.com>
2022-09-01 11:27:09 +0100

    good

--- pages/nftpage/[tokenId].jsx
+++ pages/nftpage/[tokenId].jsx
@@ -1,87 +1,9 @@
-import React, { useEffect } from "react";
-import axios from "axios";
-import { useState } from "react";
-import { useRouter } from "next/router";
-import Market from "../../engine/Market.json";
-import { hhmarketaddress, hhnftaddress } from "../../engine/configuration";
 
-const NFTPage = (props) => {
-  const [data, updateData] = useState({});
-  const [dataFetched, updateDataFetched] = useState(false);
-  const [message, updateMessage] = useState("");
-  const [currAddress, updateCurrAddress] = useState("0x");
 
-  async function getNFTData(tokenId) {
-    const ethers = require("ethers");
-    //After adding your Hardhat network to your metamask, this code will get providers and signers
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
-    const addr = await signer.getAddress();
-    //Pull the deployed contract instance
-    let contract = new ethers.Contract(hhmarketaddress, Market, signer);
-    //create an NFT Token
-    const tokenURI = await contract.tokenURI(tokenId);
-    const listedToken = await contract.getListedTokenForId(tokenId);
-    let meta = await axios.get(tokenURI);
-    meta = meta.data;
-    console.log(listedToken);
-
-    let item = {
-      price: meta.price,
-      tokenId: tokenId,
-      seller: listedToken.seller,
-      owner: listedToken.owner,
-      image: meta.image,
-      name: meta.name,
-      description: meta.description,
-    };
-    console.log(item);
-    updateData(item);
-    updateDataFetched(true);
-    console.log("address", addr);
-    updateCurrAddress(addr);
-  }
-
-  const router = useRouter()
-  const tokenId = router.query
-  //const tokenId = params.tokenId;
-  if(!dataFetched)
-        getNFTData(tokenId);
+const NFTPage = () => {
+  
   return (
-    <div style={{ "min-height": "100vh" }}>
-      <div className="flex ml-20 mt-20">
-        <img src={data.image} alt="" className="w-2/5" />
-        <div className="text-xl ml-20 space-y-8 text-white shadow-2xl rounded-lg border-2 p-5">
-          <div>Name: {data.name}</div>
-          <div>Description: {data.description}</div>
-          <div>
-            Price: <span className="">{data.price + " ETH"}</span>
-          </div>
-          <div>
-            Owner: <span className="text-sm">{data.owner}</span>
-          </div>
-          <div>
-            Seller: <span className="text-sm">{data.seller}</span>
-          </div>
-          <div>
-            {currAddress == data.owner || currAddress == data.seller ? (
-              <button
-                className="enableEthereumButton bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-sm"
-                onClick={() => buyNFT(tokenId)}
-              >
-                Buy this NFT
-              </button>
-            ) : (
-              <div className="text-emerald-700">
-                You are the owner of this NFT
-              </div>
-            )}
-
-            <div className="text-green text-center mt-3">{message}</div>
-          </div>
-        </div>
-      </div>
-    </div>
+   <h3>details</h3>
   );
 };
 
